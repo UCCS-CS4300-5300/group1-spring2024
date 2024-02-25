8726ba0433a2de4e1482fe900f0d5d6d46417694
lzeien <31412582-lzeien@users.noreply.replit.com>
2024-02-25 03:25:55 +0000

    Git issues, so only I (logan) am pushing but everyone is contributing. Got model <-> view <-> template working, now have to prettify it

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from geopy.geocoders import Nominatim
+import requests
+from datetime import datetime
 
 
 class Location(models.Model):
@@ -7,16 +10,156 @@ class Location(models.Model):
   def __str__(self):
     return self.name
 
+
 class Weather(models.Model):
-  
+
   location = models.ForeignKey(Location, on_delete=models.CASCADE)
   temperature = models.FloatField()
-  humidity = models.IntegerField() # Convert to percentage later 
+  humidity = models.IntegerField()  # Convert to percentage later
   wind_speed = models.FloatField()
   temperature_description = models.CharField(max_length=100)
   image = models.ImageField(upload_to='weather_images')
   date = models.DateTimeField()
 
   def __str__(self):
-    return self.location
+    return str(self.location)
 
+  def _get_weather(latitude, longitude):
+    """
+    Leverages api.weather.gov, a free weather API providing forecasting services. URL: https://www.weather.gov/documentation/services-web-api
+    Gets the 3 letter station closest to the user's location (location_data), and then calls that station to get the local forecast hourly (weather_data). 
+
+    Example return of the function:
+
+    "periods": [
+    {
+      "number": 1,
+      "name": "",
+      "startTime": "2024-02-24T18:00:00-07:00",
+      "endTime": "2024-02-24T19:00:00-07:00",
+      "isDaytime": false,
+      "temperature": 54,
+      "temperatureUnit": "F",
+      "temperatureTrend": null,
+      "probabilityOfPrecipitation": {
+        "unitCode": "wmoUnit:percent",
+        "value": 0
+      },
+      "dewpoint": {
+        "unitCode": "wmoUnit:degC",
+        "value": -11.666666666666666
+      },
+      "relativeHumidity": {
+        "unitCode": "wmoUnit:percent",
+        "value": 18
+      },
+      "windSpeed": "10 mph",
+      "windDirection": "WNW",
+      "icon": "https://api.weather.gov/icons/land/night/bkn,0?size=small",
+      "shortForecast": "Mostly Cloudy",
+      "detailedForecast": ""
+    },
+    """
+
+    base_url = f"https://api.weather.gov/points/{latitude},{longitude}"
+    location_response = requests.get(base_url)
+    location_data = location_response.json()
+
+    weather_response = requests.get(
+        location_data['properties']['forecastHourly'])
+    weather_data = weather_response.json()
+    return weather_data["properties"]["periods"]  # the actual weather forecast
+
+  def _format_response(weather_data):
+    """
+    Takes a format such as:
+
+    "periods": [
+      {
+        "number": 1,
+        "name": "",
+        "startTime": "2024-02-24T18:00:00-07:00",
+        "endTime": "2024-02-24T19:00:00-07:00",
+        "isDaytime": false,
+        "temperature": 54,
+        "temperatureUnit": "F",
+        "temperatureTrend": null,
+        "probabilityOfPrecipitation": {
+          "unitCode": "wmoUnit:percent",
+          "value": 0
+        },
+        "dewpoint": {
+          "unitCode": "wmoUnit:degC",
+          "value": -11.666666666666666
+        },
+        "relativeHumidity": {
+          "unitCode": "wmoUnit:percent",
+          "value": 18
+        },
+        "windSpeed": "10 mph",
+        "windDirection": "WNW",
+        "icon": "https://api.weather.gov/icons/land/night/bkn,0?size=small",
+        "shortForecast": "Mostly Cloudy",
+        "detailedForecast": ""
+      },
+
+    And converts it to:
+
+    result = {
+      "hours": [1-24],
+      "temperature": [],
+      "precipitation": [],
+      "humidity": [],
+      "wind": []
+    }
+
+    """
+
+    hours = [
+        datetime.fromisoformat(start_time["startTime"]).hour
+        for start_time in weather_data
+    ]
+    temperature = [temp["temperature"] for temp in weather_data]
+    precipitation = [
+        prec["probabilityOfPrecipitation"]["value"] for prec in weather_data
+    ]
+    humidity = [humid["relativeHumidity"]["value"] for humid in weather_data]
+    windSpeed = [
+        windSpeed["windSpeed"].split(" ")[0] for windSpeed in weather_data
+    ]
+
+    result = {
+        "hours": hours,
+        "temperature": temperature,
+        "precipitation": precipitation,
+        "humidity": humidity,
+        "wind": windSpeed
+    }
+
+    return result
+
+  @staticmethod  # similar to static methods
+  def get_weather_forecast(location):
+    """
+    Gets the hourly forecast for the next 24 hours based on the user's location, or defaults to UCCS main campus if none provided.
+    """
+    
+    #Maps API is Nomination OpenSource
+    #https://nominatim.org
+
+    # Instantiate a new Nominatim client
+    app = Nominatim(user_agent="Weather App")
+
+    location_input = "80918" if not location else location  # 80918 is UCCS main campus
+
+    # Get location raw data from the user
+    location = app.geocode(location_input)
+    if location:
+      latitude = location.latitude
+      longitude = location.longitude
+
+      weather_data = Weather._get_weather(latitude, longitude)
+      if weather_data:
+        result = Weather._format_response(weather_data)
+        return result
+    return None
--- weather_app/views.py
+++ weather_app/views.py
@@ -7,42 +7,63 @@ from datetime import datetime
 # index home page
 class WeatherView(View):
   def get(self, request):
-    # filter by day and location
-    # day is implied to be today
-    # location depends on how the frontend sends it 
+    # get location from request, e.g. /?location=80918
+    location = request.GET.get('location')
     
-    photo_path = weather.photo.path # path to image folder
-    # switch for pulling images of the weather
-    match weather_type:
-      case "Sunny":
-        weather.photo.name = "images/sunny.jpg"
-      case "Cloudy":
-        weather.photo.name = "images/cloudy.jpg"
-      case "Partly Cloudy":
-        weather.photo.name = "images/partly-cloudy.jpg"
-      case "Rain":
-        weater.photo.name = "images/rain.jpg"
-      case "Snow":
-        weather.photo.name = "images/snow.jpg"
-      case "Thunderstorm":
-        weather.photo.name = "images/thunderstorm.jpg"
-      case "Hail":
-        weather.photo.name = "images/hail.jpg"
-      case "Fog":
-        weather.photo.name = "images/fog.jpg"
+    # get weather data
+    weather_data = Weather.get_weather_forecast(location)
 
-    new_path = settings.MEDIA.ROOT + weather.photo.name
-    os.rename(photo_path, new_path) # moves the file on the file system
-    weather.save()
-    weather.photo.path == new_path
+    # template is expecting dictionary with following values
+    context = {
+      'temp_forecast': weather_data['temperature'],
+      'precipitation_forecast': weather_data['precipitation'],
+      'humidity_forecast': weather_data['humidity'],
+      'wind_forecast': weather_data['wind'],
+      'day_forecast': weather_data['hours'],
+    }
+
+    return render(request, 'weather_app/index.html', context)
+      
+    
+    # # I COMMENTED THIS OUT FOR THE TIME BEING, CHANGE IT BACK WHEN YOU WORK ON IT
+  # # I DO NOT THINK THIS FUNCTIONALITY IS NEEDED FOR SPRINT0-3, BUT MAY BE INTERESTING FOR ITERATION 1 :)!
+  # def get(self, request):
+  #   # filter by day and location
+  #   # day is implied to be today
+  #   # location depends on how the frontend sends it 
+    
+  #   photo_path = weather.photo.path # path to image folder
+  #   # switch for pulling images of the weather
+  #   match weather_type:
+  #     case "Sunny":
+  #       weather.photo.name = "images/sunny.jpg"
+  #     case "Cloudy":
+  #       weather.photo.name = "images/cloudy.jpg"
+  #     case "Partly Cloudy":
+  #       weather.photo.name = "images/partly-cloudy.jpg"
+  #     case "Rain":
+  #       weater.photo.name = "images/rain.jpg"
+  #     case "Snow":
+  #       weather.photo.name = "images/snow.jpg"
+  #     case "Thunderstorm":
+  #       weather.photo.name = "images/thunderstorm.jpg"
+  #     case "Hail":
+  #       weather.photo.name = "images/hail.jpg"
+  #     case "Fog":
+  #       weather.photo.name = "images/fog.jpg"
+
+  #   new_path = settings.MEDIA.ROOT + weather.photo.name
+  #   os.rename(photo_path, new_path) # moves the file on the file system
+  #   weather.save()
+  #   weather.photo.path == new_path
         
-    #location = request.GET.get('location') # Get location from request body
-    # Parse it into whatever format we need to filter our weather by
+  #   #location = request.GET.get('location') # Get location from request body
+  #   # Parse it into whatever format we need to filter our weather by
     
-    # Below assumes we pull weather for the whole day but we can modify it later as needed by changing the timedelta values
-    # idk how to filter location yet since we don't know how it'll be stored
-    weather_lower_bound = datetime.date.today()
-    weather_upper_bound = datetime.date.today() + datetime.datetime.timedelta(days=1)
-    weather_list = Weather.objects.filter(date__range=(weather_lower_bound, weather_upper_bound).filter(location=location))
+  #   # Below assumes we pull weather for the whole day but we can modify it later as needed by changing the timedelta values
+  #   # idk how to filter location yet since we don't know how it'll be stored
+  #   weather_lower_bound = datetime.date.today()
+  #   weather_upper_bound = datetime.date.today() + datetime.datetime.timedelta(days=1)
+  #   weather_list = Weather.objects.filter(date__range=(weather_lower_bound, weather_upper_bound).filter(location=location))
 
-    return render(request, 'weather_app/templates/weather_app/weather.html', {'weather_list': weather_list})
+  #   return render(request, 'weather_app/templates/weather_app/weather.html', {'weather_list': weather_list})
