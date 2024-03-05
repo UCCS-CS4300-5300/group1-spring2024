9108e8d64ef873148d66b9e35e48e0074ad38ed6
lzeien <31412582-lzeien@users.noreply.replit.com>
2024-03-05 01:39:55 +0000

    Logan: Created GenericClothes model with class methods to calculate the wind chill and heat index adjusted comfort value, and to return the types of clothes with those values. Included 10 unit tests for those functions and validations

--- weather_app/models.py
+++ weather_app/models.py
@@ -2,6 +2,114 @@ from django.db import models
 from geopy.geocoders import Nominatim
 import requests
 from datetime import datetime
+from django.core import validators
+
+
+# https://docs.djangoproject.com/en/5.0/howto/initial-data/#:~:text=You%20can%20load%20data%20by,and%20reloaded%20into%20the%20database to populate the list of generic clothes
+# Must be loaded manually using python manage.py loaddata fixture_generic_clothes.json
+class GenericClothes(models.Model):
+  """
+  A class holding information relating to generic clothing, such as t-shirt, cargo pants, jeans, etc. 
+  The fields are broken into: 
+  1. the type of clothing (headwear, shirts, pants, shoes, misc. for umbrellas or anything I missed)
+  2. a comfort range based on the type of clothing (e.g. jeans, cargo pants, etc.) and a custom comfort calculation as follows:
+
+  Comfort = {HI, T > 75, WC, T <= 75} - TOLERANCE_offset + WORKING_offset
+  HI = 0.5 * {T + 61.0 + [(T-68.0)1.2] + (RH * 0.094)}
+  WC = 35.74 + 0.6215T - 35.75v^{0.16} + 0.4275Tv^{0.16}, where v is the wind speed in MPH
+  
+  3. a waterproof rating.
+  """
+
+  # Enforces the category of clothing with choices, https://docs.djangoproject.com/en/5.0/ref/models/fields/#charfield
+  choices = {
+      "HAT": "Hat",
+      "SHR": "Shirts and Jackets",
+      "PNT": "Pants",
+      "SHO": "Shoes",
+      "MIS": "Miscellaneous"
+  }
+
+  name = models.CharField(max_length=50)
+  clothing_type = models.CharField(max_length=3, choices=choices)
+  comfort_low = models.IntegerField()
+  comfort_high = models.IntegerField()
+  waterproof_rating = models.IntegerField()  # percentage from 1-100
+
+  def __str__(self):
+    return self.name
+
+  def clean(self, *args, **kwargs):
+    """
+    Django’s form (and model) fields support use of utility functions and classes known as validators. A validator is a callable object or function that takes a value and returns nothing if the value is valid or raises a ValidationError if not - https://docs.djangoproject.com/en/5.0/ref/validators/
+
+    Here, we ensure that the comfort low is not greater than the high
+    """
+    super().clean(
+        *args, **kwargs
+    )  # see https://stackoverflow.com/questions/7366363/adding-custom-django-model-validation
+
+    if self.comfort_low >= self.comfort_high:
+      raise validators.ValidationError(
+          "Comfort low must be less than or equal to comfort high.")
+
+  @classmethod
+  def calculate_comfort(cls, temperature, humidity, wind_speed,
+                        tolerance_offset, working_offset):
+    """
+    Calculate the comfort level based on the temperature, humidity, and wind speed. Less of a comfort calculation and more of a adjusted feels-like temperature.
+
+    Params:
+    * Temperature in farenheit
+    * Humidity in percentage from 0-100+ (technically humidity can go beyond 100%)
+    * Wind speed in miles per hour
+    """
+
+    if humidity < 0:
+      raise ValueError("Humidity must be greater than or equal to 0.")
+
+    if wind_speed < 0:
+      raise ValueError("Wind speed must be greater than or equal to 0.")
+
+    if tolerance_offset < 0 or working_offset < 0:
+      raise ValueError(
+          "Tolerance offset and working offset must be greater than or equal to 0."
+      )
+
+    # Calculate the adjusted feels-like temperature
+    if temperature > 75:
+      # equation from https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+      heat_index = -42.379 + 2.04901523 * temperature + 10.14333127 * humidity - 0.22475541 * temperature * humidity - 0.00683783 * temperature * temperature - 0.05481717 * humidity * humidity + 0.00122874 * temperature * temperature * humidity + 0.00085282 * temperature * humidity * humidity - 0.00000199 * temperature * temperature * humidity * humidity
+      return heat_index - tolerance_offset + working_offset
+
+    # If the temperature is less than or equal to 75, use the wind chill
+    else:
+      # equation from https://en.wikipedia.org/wiki/Wind_chill NA wind chill index
+      wind_chill = 35.74 + 0.6215 * temperature - 35.75 * (
+          wind_speed**0.16) + 0.4275 * temperature * (wind_speed**0.16)
+      return wind_chill - tolerance_offset + working_offset
+
+  @classmethod
+  def get_clothes_in_range(cls, comfort):
+    """
+    Function that, given a comfort value, iterates through the current clothes in the database and returns the first set of clothes that are within the comfort range.
+
+    Currently does not include an umbrella or waterproofing.
+    """
+
+    # comfort_low <= comfort <= comfort_high
+    clothes = cls.objects.filter(comfort_low__lte=comfort,
+                                 comfort_high__gte=comfort)
+    # build the outfit, little bit of metaprogramming / syntactic sugar
+    try:
+      outfit = [
+          clothes.filter(clothing_type=query)[0]
+          for query in ["HAT", "SHR", "PNT", "SHO"]
+      ]
+    except:  # [0] throws error when filter returns nothing
+      outfit = []
+
+    return outfit
 
 
 class Location(models.Model):
@@ -143,7 +251,7 @@ class Weather(models.Model):
     """
     Gets the hourly forecast for the next 24 hours based on the user's location, or defaults to UCCS main campus if none provided.
     """
-    
+
     #Maps API is Nomination OpenSource
     #https://nominatim.org
 
