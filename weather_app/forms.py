6d16a89421b814d7da90a352cfc230971695e398
Srikanth020698 <mudunurisrikanth98@gmail.com>
2024-04-16 15:31:18 -0600

    resovled conflicts

--- weather_app/forms.py
+++ weather_app/forms.py
@@ -12,15 +12,9 @@ class CreateUserForm(UserCreationForm):
     fields = ['username', 'email', 'password1', 'password2']
 
 class AddForm(ModelForm):
-<<<<<<< HEAD
-    class Meta:
-        model = GenericClothes
-        fields = ('name', 'clothing_type', 'comfort_low', 'comfort_high', 'waterproof_rating', 'photo', 'image_url')
-=======
   """
   Forms for adding items to the inventory
   """
-  class Meta:
-    model = GenericClothes
-    fields = ('name', 'clothing_type', 'comfort_low', 'comfort_high', 'waterproof_rating')
->>>>>>> eabd4f6504cc42318f24cee13260a2f0c2f7e6ac
+    class Meta:
+        model = GenericClothes
+        fields = ('name', 'clothing_type', 'comfort_low', 'comfort_high', 'waterproof_rating', 'photo', 'image_url')
