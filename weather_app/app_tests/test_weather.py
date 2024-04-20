fe59a50922e3161fcbdd840bea6d7f67098ea592
LoganZeien <lzeien@uccs.edu>
2024-04-20 17:02:33 +0000

    Logan: Finished unit tests for weather model, going to view

--- weather_app/app_tests/test_weather.py
+++ weather_app/app_tests/test_weather.py
@@ -2,9 +2,10 @@
 Tests for the Weather model, View, and API calls.
 """
 
-from unittest.mock import patch, call
+from unittest.mock import patch, call, PropertyMock
 from django.test import TestCase
 from requests import ConnectTimeout, Response
+from geopy.location import Location
 from ..models import Weather
 from ..views import WeatherView
 from .. import models
@@ -193,6 +194,226 @@ class TestWeatherUnitTest(TestCase):
 
     output = Weather._format_response(None, None)
     self.assertEqual(output, {})
+
+  # ---------------------------------------------------------------------------------
+
+  # --------------------------- get_weather_forecast ---------------------------
+
+  def test_get_weather_forecast(self):
+    """
+    Tests get_weather_forecast(location)
+    * Ensures it can correctly get latitude and longitude then call 
+    * _get_weather and _format_response
+    """
+
+    location = "New York"
+
+    mock_get_weather_return = [
+      {
+        "number": 1,
+        "name": "",
+        "startTime": "2024-02-24T18:00:00-07:00",
+        "endTime": "2024-02-24T19:00:00-07:00",
+        "temperature": 10,
+        "temperatureUnit": "F",
+        "probabilityOfPrecipitation": {
+          "unitCode": "wmoUnit:percent",
+          "value": 90
+        },
+        "dewpoint": {
+          "unitCode": "wmoUnit:degC",
+          "value": -11.666666666666666
+        },
+        "relativeHumidity": {
+          "unitCode": "wmoUnit:percent",
+          "value": 30
+        },
+        "windSpeed": "10 mph",
+        "windDirection": "WNW",
+        "icon": "https://api.weather.gov/icons/land/night/bkn,0?size=small",
+        "shortForecast": "Mostly Cloudy",
+        "detailedForecast": ""
+      },
+      {
+        "number": 1,
+        "name": "",
+        "startTime": "2024-02-24T19:00:00-07:00",
+        "endTime": "2024-02-24T20:00:00-07:00",
+        "temperature": 20,
+        "temperatureUnit": "F",
+        "probabilityOfPrecipitation": {
+          "unitCode": "wmoUnit:percent",
+          "value": 100
+        },
+        "dewpoint": {
+          "unitCode": "wmoUnit:degC",
+          "value": -11.666666666666666
+        },
+        "relativeHumidity": {
+          "unitCode": "wmoUnit:percent",
+          "value": 40
+        },
+        "windSpeed": "25 mph",
+        "windDirection": "WNW",
+        "icon": "https://api.weather.gov/icons/land/night/bkn,0?size=small",
+        "shortForecast": "Mostly Cloudy",
+        "detailedForecast": ""
+      }
+    ]
+
+    mock_format_response = {
+      "hours": [18, 19],
+      "temperature": [10, 20],
+      "precipitation": [90, 100],
+      "humidity": [30, 40],
+      "wind": [10, 25],
+      "location": "New York"
+    }
+
+    class MockLocation():
+      def __init__(self, address, coords, raw):
+        self.address = address
+        self.latitude = coords[0]
+        self.longitude = coords[1]
+        self.raw = raw
+
+    mock_geocode_return = MockLocation("New Work", (40.7127281, -74.0060152), "raw")
+    
+    expected_get_weather_calls = [
+      call(40.7127281, -74.0060152)
+    ]
+
+    expected_get_format_response_calls = [
+      call(mock_get_weather_return, mock_geocode_return)
+    ]
+    
+    with (
+      patch('weather_app.models.Weather._get_weather') as mock_get_weather,
+      patch('weather_app.models.Weather._format_response') as mock_format_response,
+      patch('geopy.geocoders.Nominatim.geocode') as mock_geocode
+    ):
+      mock_get_weather.return_value = mock_get_weather_return
+      mock_format_response.return_value = mock_format_response
+      mock_geocode.return_value = mock_geocode_return
+
+      forecast = Weather.get_weather_forecast(location)
+
+      mock_get_weather.assert_has_calls(expected_get_weather_calls)
+      mock_format_response.assert_has_calls(expected_get_format_response_calls)
+      self.assertEqual(forecast, mock_format_response.return_value)
+
+  def test_get_weather_forecast_invalid_input(self):
+    """
+    Tests get_weather_forecast(location)
+    * Ensures it returns none if poorly formatted input (e.g., canada)
+    """
+
+    location = "Canada"
+    output = Weather.get_weather_forecast(location)
+    self.assertEqual(output, None)
+
+  def test_get_weather_forecast_default_input(self):
+    """
+    Tests get_weather_forecast(location)
+    * Ensures it uses default location if none provided
+    """
+  
+    location = ""
+
+    mock_get_weather_return = [
+      {
+        "number": 1,
+        "name": "",
+        "startTime": "2024-02-24T18:00:00-07:00",
+        "endTime": "2024-02-24T19:00:00-07:00",
+        "temperature": 10,
+        "temperatureUnit": "F",
+        "probabilityOfPrecipitation": {
+          "unitCode": "wmoUnit:percent",
+          "value": 90
+        },
+        "dewpoint": {
+          "unitCode": "wmoUnit:degC",
+          "value": -11.666666666666666
+        },
+        "relativeHumidity": {
+          "unitCode": "wmoUnit:percent",
+          "value": 30
+        },
+        "windSpeed": "10 mph",
+        "windDirection": "WNW",
+        "icon": "https://api.weather.gov/icons/land/night/bkn,0?size=small",
+        "shortForecast": "Mostly Cloudy",
+        "detailedForecast": ""
+      },
+      {
+        "number": 1,
+        "name": "",
+        "startTime": "2024-02-24T19:00:00-07:00",
+        "endTime": "2024-02-24T20:00:00-07:00",
+        "temperature": 20,
+        "temperatureUnit": "F",
+        "probabilityOfPrecipitation": {
+          "unitCode": "wmoUnit:percent",
+          "value": 100
+        },
+        "dewpoint": {
+          "unitCode": "wmoUnit:degC",
+          "value": -11.666666666666666
+        },
+        "relativeHumidity": {
+          "unitCode": "wmoUnit:percent",
+          "value": 40
+        },
+        "windSpeed": "25 mph",
+        "windDirection": "WNW",
+        "icon": "https://api.weather.gov/icons/land/night/bkn,0?size=small",
+        "shortForecast": "Mostly Cloudy",
+        "detailedForecast": ""
+      }
+    ]
+
+    mock_format_response = {
+      "hours": [18, 19],
+      "temperature": [10, 20],
+      "precipitation": [90, 100],
+      "humidity": [30, 40],
+      "wind": [10, 25],
+      "location": "80918"
+    }
+
+    class MockLocation():
+      def __init__(self, address, coords, raw):
+        self.address = address
+        self.latitude = coords[0]
+        self.longitude = coords[1]
+        self.raw = raw
+
+    mock_geocode_return = MockLocation("80918", (40.7127281, -74.0060152), "raw")
+
+    expected_get_weather_calls = [
+      call(40.7127281, -74.0060152)
+    ]
+
+    expected_get_format_response_calls = [
+      call(mock_get_weather_return, mock_geocode_return)
+    ]
+
+    with (
+      patch('weather_app.models.Weather._get_weather') as mock_get_weather,
+      patch('weather_app.models.Weather._format_response') as mock_format_response,
+      patch('geopy.geocoders.Nominatim.geocode') as mock_geocode
+    ):
+      mock_get_weather.return_value = mock_get_weather_return
+      mock_format_response.return_value = mock_format_response
+      mock_geocode.return_value = mock_geocode_return
+
+      forecast = Weather.get_weather_forecast(location)
+
+      mock_get_weather.assert_has_calls(expected_get_weather_calls)
+      mock_format_response.assert_has_calls(expected_get_format_response_calls)
+      self.assertEqual(forecast, mock_format_response.return_value)
+
 
   # ---------------------------------------------------------------------------------
 
